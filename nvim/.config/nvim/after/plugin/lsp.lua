d63a9c3f7d8c6113dc750518f40cc85f0a8dd3dd
Tomás Esteves <tomasesteves2002@gmail.com>
2023-10-29 12:52:44 +0000

    Add copilot

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -38,9 +38,9 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<S-tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-CR>'] = cmp.mapping.complete(),
   }),
 })
--- nvim/.config/nvim/lua/nvimconfig/remap.lua
+++ nvim/.config/nvim/lua/nvimconfig/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.g.copilot_no_tab_map = true
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {noremap = true,expr=true, silent=true, replace_keycodes = false})
