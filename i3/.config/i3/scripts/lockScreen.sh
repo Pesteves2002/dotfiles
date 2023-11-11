cae0df94022278a38fab572ec7c66eb702f62729
Tomás Esteves <tomasesteves2002@gmail.com>
2023-11-11 21:07:42 +0000

    Change screenlocker

--- i3/.config/i3/scripts/lockScreen.sh
+++ i3/.config/i3/scripts/lockScreen.sh
@@ -1,20 +1,54 @@
 #!/bin/sh
 
-greet="Welcome back, Sir!"
-correct="yey!"
-color="ffffff"
-wrong_pass="WRONG"
-font="JetBrains Mono"
-background="$HOME/dotfiles/wallpapers/10.jpg"
+alpha='dd'
+background='#282a36'
+selection='#44475a'
+comment='#6272a4'
+
+yellow='#f1fa8c'
+orange='#ffb86c'
+red='#ff5555'
+magenta='#ff79c6'
+blue='#6272a4'
+cyan='#8be9fd'
+green='50fa7b'
 
 i3lock \
-  -C \
-  -f \
-  --nofork \
-  --ignore-empty-password \
+  --insidever-color=$selection$alpha \
+  --insidewrong-color=$selection$alpha \
+  --inside-color=$selection$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ring-color=$blue$alpha \
+  --line-uses-ring \
+  --keyhl-color=$magenta$alpha \
+  --bshl-color=$orange$alpha \
+  --separator-color=$selection$alpha \
+  --verif-color=$green \
+  --wrong-color=$red \
+  --layout-color=$blue \
+  --date-color=$blue \
+  --time-color=$blue \
+  --screen 1 \
+  --blur 1 \
+  --clock \
   --indicator \
-  --ringver-color=$color \
-  --ringwrong-color=$color \
-  --bshl-color=$color \
-  --keyhl-color=$color \
-  -i $background
+  --time-str="%H:%M:%S" \
+  --date-str="%A %e %B %Y" \
+  --verif-text="Checking..." \
+  --wrong-text="Wrong" \
+  --noinput="No Input" \
+  --lock-text="Locking..." \
+  --lockfailed="Lock Failed" \
+  --radius=120 \
+  --ring-width=10 \
+  --pass-media-keys \
+  --pass-screen-keys \
+  --pass-volume-keys \
+  --time-font="JetBrainsMono Nerd Font" \
+  --date-font="JetBrainsMono Nerd Font" \
+  --layout-font="JetBrainsMono Nerd Font" \
+  --verif-font="JetBrainsMono Nerd Font" \
+  --wrong-font="JetBrainsMono Nerd Font" \
