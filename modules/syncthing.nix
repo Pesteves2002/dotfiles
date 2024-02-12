2540ec7dbe700f3bef643fc3e22d6760a79d5094
Tomás Esteves <tomasesteves2002@gmail.com>
2024-02-12 20:58:04 +0000

    fix: syncthing override GUI data

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -4,6 +4,11 @@
     user = "tomas";
     # TODO: Remove hardcoded
     dataDir = "/home/tomas/.syncthing";
+
+    # Do not allow Syncthing to remove folders and devices configured with the GUI
+    overrideFolders = false;
+    overrideDevices = false;
+
     settings = {gui = {theme = "dark";};};
   };
 }
