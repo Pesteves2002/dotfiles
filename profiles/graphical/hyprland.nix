ffe934008eeaf4db8d0ec5577d232c9204d0822d
Tomás Esteves <tomasesteves2002@gmail.com>
2024-02-13 19:19:00 +0000

    feat: add keyboard music keys

--- profiles/graphical/hyprland.nix
+++ profiles/graphical/hyprland.nix
@@ -110,6 +110,12 @@
         "$mod, D, exec, pkill wofi || wofi --show=drun"
       ];
 
+      binde = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ];
+
       binds = {allow_workspace_cycles = true;};
 
       misc = {disable_hyprland_logo = true;};
