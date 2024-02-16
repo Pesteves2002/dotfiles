c1c2dbd8bcf6e3ef124f281de89ecdb5b5a3dd96
Tomás Esteves <tomasesteves2002@gmail.com>
2024-02-16 21:58:46 +0000

    feat: improve nixvim config

--- profiles/nixvim/default.nix
+++ profiles/nixvim/default.nix
@@ -5,6 +5,15 @@
     viAlias = true;
     vimAlias = true;
 
+    autoCmd = [
+      # save position in file (cursed thingies)
+      {
+        event = "BufReadPost";
+        pattern = "*";
+        callback = {__raw = "function() vim.api.nvim_exec('silent! normal! g`\"zv', false) end";};
+      }
+    ];
+
     globals.mapleader = " ";
 
     options = {
@@ -33,6 +42,9 @@
 
       spell = true;
       spelllang = ["en" "pt"];
+
+      undofile = true;
+      undodir = "/tmp//";
     };
 
     clipboard.register = "unnamedplus";
@@ -41,6 +53,64 @@
     colorschemes.one.enable = true;
 
     keymaps = [
+      # invert search direction
+      {
+        mode = "n";
+        key = "n";
+        action = "'Nn'[v:searchforward]";
+        options.expr = true;
+      }
+      {
+        mode = "n";
+        key = "N";
+        action = "'nN'[v:searchforward]";
+        options.expr = true;
+      }
+      # clear search highlight
+      {
+        mode = "n";
+        key = "<Esc>";
+        action = ":nohlsearch<CR>";
+        options.silent = true;
+      }
+
+      # toggle wrap mode
+      {
+        mode = "n";
+        key = "<Leader>w";
+        action = ":set wrap!<CR>";
+      }
+
+      # move lines up and down
+
+      {
+        mode = "v";
+        key = "J";
+        action = ":m '>+1<CR>gv=gv";
+      }
+      {
+        mode = "v";
+        key = "K";
+        action = ":m '<-2<CR>gv=gv";
+      }
+
+      {
+        mode = "t";
+        key = "<Esc>";
+        action = "<C-\\><C-n>";
+      }
+
+      # save methods
+      {
+        mode = "n";
+        key = "<C-s>";
+        action = ":w<CR>";
+      }
+      {
+        mode = "i";
+        key = "<C-s>";
+        action = "<Esc>:w<CR>a";
+      }
     ];
 
     plugins = {
@@ -89,6 +159,20 @@
       gitblame = {
         enable = true;
       };
+
+      presence-nvim = {
+        enable = true;
+        editingText = "Martelating %s";
+        workspaceText = "Breaking the code of %s";
+      };
     };
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  systemd.user.sessionVariables = {
+    EDITOR = "nvim";
   };
 }
