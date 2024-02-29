3c6a0290d6807023ff1c20b6056bd784fe420ec6
Tomás Esteves <tomasesteves2002@gmail.com>
2024-02-29 15:55:55 +0000

    feat: add runner to SEC project

--- profiles/github-runner.nix
+++ profiles/github-runner.nix
@@ -6,5 +6,11 @@
       tokenFile = "/home/tomas/Documents/CPD/runner.token";
       url = "https://github.com/Pesteves2002/game-of-life-3d-cpd";
     };
+    runner2 = {
+      enable = true;
+      name = "sec-runner";
+      tokenFile = "/home/tomas/Documents/SEC/runner.token";
+      url = "https://github.com/Pesteves2002/HDSLedger";
+    };
   };
 }
