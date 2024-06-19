9c69d91831d2125e68acfc2e5ccace21a7080db0
Tomás Esteves <tomasesteves2002@gmail.com>
2024-06-19 14:57:45 +0100

    feat: add timeout to boot loader

--- hosts/novablast/default.nix
+++ hosts/novablast/default.nix
@@ -15,8 +15,15 @@
   ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      consoleMode = "auto";
+    };
+
+    efi.canTouchEfiVariables = true;
+    timeout = 3;
+  };
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
