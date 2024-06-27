a052af37f75ea6e8a344b0adfda8053e0956e53d
Tomás Esteves <tomasesteves2002@gmail.com>
2024-06-27 01:19:08 +0100

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
