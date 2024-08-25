5d5bf43d39bfd9351ec4b485c09702b757382d6c
Tomas Esteves <tomasesteves2002@gmail.com>
2024-08-25 21:52:29 +0200

    feat: add suspend to dragonfly

--- home/tomas/features/graphical/hyprland/hypridle.nix
+++ home/tomas/features/graphical/hyprland/hypridle.nix
@@ -1,41 +1,61 @@
 {
   lib,
+  pkgs,
   config,
   ...
-}: {
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof ${lib.getExe config.programs.hyprlock.package} || ${lib.getExe config.programs.hyprlock.package}"; # avoid starting multiple hyprlock instances.
-        before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-        after_sleep_cmd = "${lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl"} dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+}: let
+  hyprlock = lib.getExe pkgs.hyprlock;
+  loginctl = lib.getExe' pkgs.systemd "loginctl";
+  systemctl = lib.getExe' pkgs.systemd "systemctl";
+  hyprctl = lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl";
+  cfg = config.services.hypridle;
+in {
+  options.services.hypridle.enableSuspend = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Enable system suspend by inactivity.";
+  };
+
+  config = {
+    services.hypridle = {
+      enable = true;
+
+      settings = {
+        general = {
+          lock_cmd = "pidof hyprlock || ${hyprlock}"; # lock screen.
+          after_sleep_cmd = "${hyprctl} dispatch dpms on"; # turn on display after resume.
+          before_sleep_cmd = "${loginctl} lock-session"; # lock before suspend.
+        };
+
+        listener =
+          [
+            {
+              timeout = 150;
+              on-timeout = "brightnessctl -s set 10";
+              on-resume = "brightnessctl -r";
+            }
+            {
+              timeout = 150;
+              on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+              on-resume = "brightnessctl -rd rgb:kbd_backlight";
+            }
+            {
+              timeout = 300;
+              on-timeout = "${loginctl} lock-session";
+            }
+            {
+              timeout = 310;
+              on-timeout = "${hyprctl} dispatch dpms off";
+              on-resume = "${hyprctl} dispatch dpms on";
+            }
+          ]
+          ++ lib.optionals cfg.enableSuspend [
+            {
+              timeout = 330;
+              on-timeout = "${systemctl} suspend";
+            }
+          ];
       };
-
-      listener = [
-        {
-          timeout = 150; # 2.5min.
-          on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
-          on-resume = "brightnessctl -r"; # monitor backlight restore.
-        }
-
-        {
-          timeout = 150; # 2.5min.
-          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
-          on-resume = "brightnessctl -rd rgb:kbd_backlight"; # turn on keyboard backlight.
-        }
-
-        {
-          timeout = 300; # 5min
-          on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
-        }
-
-        {
-          timeout = 330; # 5.5min
-          on-timeout = "${lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl"} dispatch dpms off"; # screen off when timeout has passed
-          on-resume = "${lib.getExe' config.wayland.windowManager.hyprland.package "hyprctl"} dispatch dpms on"; # screen on when activity is detected after timeout has fired.
-        }
-      ];
     };
   };
 }
--- home/tomas/features/graphical/hyprland/laptop.nix
+++ home/tomas/features/graphical/hyprland/laptop.nix
@@ -19,4 +19,6 @@
       misc.vfr = true;
     };
   };
+
+  services.hypridle.enableSuspend = true;
 }
