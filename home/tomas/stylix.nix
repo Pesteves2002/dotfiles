894e03e40e4ec7aec0823b6fc44093ad9db21996
Tomas Esteves <tomasesteves2002@gmail.com>
2024-10-16 23:41:52 +0200

    feat: add fonts to stylix

--- home/tomas/stylix.nix
+++ home/tomas/stylix.nix
@@ -6,10 +6,42 @@
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-gray.yaml";
 
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.iosevka;
+        name = "IosevkaTerm";
+      };
+
+      sansSerif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Sans";
+      };
+
+      serif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
+      };
+
+      sizes = {
+        applications = 12;
+        terminal = 12;
+        desktop = 10;
+        popups = 10;
+      };
+    };
+
+    polarity = "dark";
+
     targets = {
       hyprland.enable = false;
       nixvim.enable = false;
-waybar.enable = false;
+      waybar.enable = false;
       wofi.enable = true;
     };
   };
