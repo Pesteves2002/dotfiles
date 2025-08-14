d42188731a579722983e206b3cb5bb5d26bc9284
Tomás Esteves <tomasesteves2002@gmail.com>
2025-08-15 00:58:19 +0100

    feat: add dns server

--- hosts/novablast/default.nix
+++ hosts/novablast/default.nix
@@ -33,7 +33,7 @@
     ipv4Address = "192.168.1.2";
     ipv4SubnetMask = 24;
     ipv4Gateway = "192.168.1.1";
-    dnsServers = ["1.1.1.1" "1.0.0.1"];
+    dnsServers = ["1.1.1.1" "1.0.0.1" "9.9.9.9"];
 
     wakeOnLan = true;
   };
