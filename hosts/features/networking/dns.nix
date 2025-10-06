e27f5f9809310bf0ebd98580c20d97bd9e814761
Tomás Esteves <tomasesteves2002@gmail.com>
2025-10-06 14:18:55 +0100

    feat: add dns over https

--- hosts/features/networking/dns.nix
+++ hosts/features/networking/dns.nix
@@ -1,5 +1,13 @@
 {config, ...}: let
   c = config.networking;
+
+  # https://dnscrypt.info/stamps/
+  upstreams = [
+    "sdns://AgcAAAAAAAAABzEuMS4xLjEAEmRucy5jbG91ZGZsYXJlLmNvbQovZG5zLXF1ZXJ5" # 1.1.1.1
+    "sdns://AgcAAAAAAAAABzEuMC4wLjEAEmRucy5jbG91ZGZsYXJlLmNvbQovZG5zLXF1ZXJ5" # 1.0.0.1
+    "sdns://AgcAAAAAAAAAFlsyNjA2OjQ3MDA6NDcwMDo6MTExMV0AIDFkb3QxZG90MWRvdDEuY2xvdWRmbGFyZS1kbnMuY29tCi9kbnMtcXVlcnk" # [2606:4700:4700::1111]
+    "sdns://AgcAAAAAAAAAFlsyNjA2OjQ3MDA6NDcwMDo6MTAwMV0AIDFkb3QxZG90MWRvdDEuY2xvdWRmbGFyZS1kbnMuY29tCi9kbnMtcXVlcnk" # [2606:4700:4700::1001]
+  ];
 in {
   # Network Manager
   networking = {
@@ -7,5 +15,16 @@ in {
 
     dhcpcd.extraConfig = "nohook resolv.conf";
     networkmanager.dns = "none";
+  };
+
+  services.dnsproxy = {
+    enable = true;
+    settings = {
+      upstream = upstreams;
+      listen-addrs = [
+        "127.0.0.53"
+      ];
+    };
+    flags = ["--cache"];
   };
 }
