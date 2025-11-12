f7d16b330aed275caa30a88ee0265224156e59be
Tomás Esteves <tomasesteves2002@gmail.com>
2025-11-12 11:55:38 +0000

    feat: update tomase_website flake lock

--- hosts/features/networking/nginx.nix
+++ hosts/features/networking/nginx.nix
@@ -1,23 +1,6 @@
 {
   services.nginx = {
     enable = true;
-    virtualHosts."tomase.pt" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        return = "200 '<html><body>It works</body></html>'";
-        extraConfig = ''
-          default_type text/html;
-        '';
-      };
-
-      locations."/test" = {
-        return = "200 '<html><body>It works too</body></html>'";
-        extraConfig = ''
-          default_type text/html;
-        '';
-      };
-    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
