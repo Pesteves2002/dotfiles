a71fbf55fcbb018417c1b15c9b75fa205cad713e
Tomás Esteves <tomasesteves2002@gmail.com>
2025-11-26 15:44:43 +0000

    feat: rollback to rust-analyzer lsp plugin

--- home/tomas/features/tools/nixvim/plugins/lsp/rust.nix
+++ home/tomas/features/tools/nixvim/plugins/lsp/rust.nix
@@ -1,33 +1,33 @@
-{
+{pkgs, ...}: {
   programs.nixvim.plugins = {
     lsp.servers = {
-      # rust_analyzer = {
-      #   enable = true;
-      #   package = pkgs.unstable.rust-analyzer;
+      rust_analyzer = {
+        enable = true;
+        package = pkgs.unstable.rust-analyzer;
 
-      #   installCargo = false;
-      #   installRustc = false;
-      #   installRustfmt = false;
+        installCargo = false;
+        installRustc = false;
+        installRustfmt = false;
 
-      #   settings = {
-      #     cargo = {
-      #       features = "all";
-      #     };
+        settings = {
+          cargo = {
+            features = "all";
+          };
 
-      #     check = {
-      #       command = "clippy";
-      #     };
+          check = {
+            command = "clippy";
+          };
 
-      #     procMacro = {
-      #       ignored = {
-      #         leptos_macro = [
-      #           # component
-      #           "server"
-      #         ];
-      #       };
-      #     };
-      #   };
-      # };
+          procMacro = {
+            ignored = {
+              leptos_macro = [
+                # component
+                "server"
+              ];
+            };
+          };
+        };
+      };
 
       # TOML LSP
       taplo = {
@@ -35,31 +35,32 @@
       };
     };
 
-    rustaceanvim = {
-      enable = true;
+    # rustaceanvim = {
+    #   enable = true;
+    #   package = pkgs.unstable.vimPlugins.rustaceanvim;
 
-      settings = {
-        default_settings = {
-          rust-analyzer = {
-            cargo = {
-              features = "all";
-            };
+    #   settings = {
+    #     default_settings = {
+    #       rust-analyzer = {
+    #         cargo = {
+    #           features = "all";
+    #         };
 
-            check = {
-              command = "clippy";
-            };
+    #         check = {
+    #           command = "clippy";
+    #         };
 
-            procMacro = {
-              ignored = {
-                leptos_macro = [
-                  # component
-                  "server"
-                ];
-              };
-            };
-          };
-        };
-      };
-    };
+    #         procMacro = {
+    #           ignored = {
+    #             leptos_macro = [
+    #               # component
+    #               "server"
+    #             ];
+    #           };
+    #         };
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
