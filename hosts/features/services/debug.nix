69e73b57368a8fb06c6f87cdb424289d36f30038
Tomás Esteves <tomasesteves2002@gmail.com>
2025-12-07 19:38:40 +0000

    feat: add postgresql debug config

--- hosts/features/services/debug.nix
+++ hosts/features/services/debug.nix
@@ -4,7 +4,36 @@
 in {
   services.postgresql = {
     enable = lib.mkDefault true;
-    ensureUsers = [{name = dbUsername;}];
+    ensureUsers = [
+      {
+        name = dbUsername;
+        ensureDBOwnership = true;
+      }
+    ];
     ensureDatabases = [dbName];
+
+    authentication = ''
+      #type database  DBuser  auth-method
+      local all      all     trust
+    '';
+
+    identMap = ''
+      # ArbitraryMapName systemUser DBUser
+         superuser_map      root      postgres
+         superuser_map      postgres  postgres
+         # Let other names login as themselves
+         superuser_map      /^(.*)$   \1
+    '';
+  };
+
+  users = {
+    users.kerneloeiras = {
+      isSystemUser = true;
+      group = "kerneloeiras";
+    };
+
+    groups = {
+      kerneloeiras = {};
+    };
   };
 }
