bd041f8e6edb73216942e8b740d2565e40661bc1
Tomás Esteves <tomasesteves2002@gmail.com>
2026-08-23 14:17:10 +0100

    feat: truncate filenames on telescope

--- home/tomas/features/tools/nixvim/plugins/telescope.nix
+++ home/tomas/features/tools/nixvim/plugins/telescope.nix
@@ -18,30 +18,16 @@
         };
       };
 
+      settings = {
+        defaults = {
+          path_display = ["truncate"];
+        };
+      };
+
       extensions = {
         ui-select.enable = true;
         fzf-native.enable = true;
       };
     };
-
-    # # Fix: https://github.com/nvim-treesitter/nvim-treesitter/issues/7952#issuecomment-2996499844
-    # extraConfigLua = ''
-    #   local actions = require("telescope.actions")
-
-    #   local open_after_tree = function(prompt_bufnr)
-    #     vim.defer_fn(function()
-    #       actions.select_default(prompt_bufnr)
-    #     end, 100) -- Delay allows filetype and plugins to settle before opening
-    #   end
-
-    #   require("telescope").setup({
-    #     defaults = {
-    #       mappings = {
-    #         i = { ["<CR>"] = open_after_tree },
-    #         n = { ["<CR>"] = open_after_tree },
-    #       },
-    #     },
-    #   })
-    # '';
   };
 }
